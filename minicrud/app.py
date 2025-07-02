177111c101c67d0739be26334ceaafbac68d0b51
Harry Richardson <richardson.chesney@gmail.com>
2025-07-02 11:53:45 -0500

    feat: Add unit tests for data blueprint

--- minicrud/app.py
+++ minicrud/app.py
@@ -3,9 +3,9 @@ This module contains the factory function for creating the Flask application.
 """
 
 from flask import Flask
-from ..database import db
-from ..config import Config
-from .blueprints.data_bp import data_bp
+from minicrud.database import db
+from minicrud.config import Config
+from minicrud.blueprints.data_bp import data_bp
 
 def create_app():
     """
@@ -19,7 +19,7 @@ def create_app():
     """
     app = Flask(__name__)
     app.config.from_object(Config)
-    db.init_app(app)
+    
 
     app.register_blueprint(data_bp)
 
